8dbd0ef58c8a083274a69ae50389445e8f68d15c
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2024-08-22 11:32:48 +0200

    exercism(elixir): one-liners

--- elixir/bird-count/lib/bird_count.ex
+++ elixir/bird-count/lib/bird_count.ex
@@ -1,13 +1,17 @@
 defmodule BirdCount do
-  def today(list) when list == [], do: nil
-  def today(list), do: hd(list)
-  def increment_day_count(list) when list == [], do: [1]
-  def increment_day_count(list), do: [hd(list) + 1 | tl(list)]
+  def today([]), do: nil
+  def today([head | _]), do: head
+
+  def increment_day_count([]), do: [1]
+  def increment_day_count([head | tail]), do: [head + 1 | tail]
+
   def has_day_without_birds?([]), do: false
-  def has_day_without_birds?(list), do: hd(list) == 0 or has_day_without_birds?(tl(list))
+  def has_day_without_birds?([head | tail]), do: head == 0 or has_day_without_birds?(tail)
+
   def total([]), do: 0
-  def total(list), do: hd(list) + total(tl(list))
+  def total([head | tail]), do: head + total(tail)
+
   def busy_days([]), do: 0
-  def busy_days(list) when hd(list) >= 5, do: 1 + busy_days(tl(list))
-  def busy_days(list), do: busy_days(tl(list))
+  def busy_days([head | tail]) when head >= 5, do: busy_days(tail) + 1
+  def busy_days([_ | tail]), do: busy_days(tail)
 end
--- elixir/german-sysadmin/lib/username.ex
+++ elixir/german-sysadmin/lib/username.ex
@@ -1,26 +1,16 @@
 defmodule Username do
-  def sanitize(username) do
-    sanitize(username, [])
-  end
-
-  # NOTE: in ascii table the underscore is placed between ?a and ?z
-  defp sanitize([fl | tail], rest) when fl >= ?a and fl <= ?z do
-    sanitize(tail, [fl | rest])
-  end
-
-  defp sanitize([fl | tail], rest) do
+  def sanitize(username), do: sanitize(username, [])
+  # NOTE: in ascii table the underscore (?_) is placed between ?a and ?z
+  defp sanitize([fl | tail], acc_list) when fl >= ?a and fl <= ?z, do: sanitize(tail, [fl | acc_list])
+  defp sanitize([], acc_list), do: acc_list |> Enum.reverse()
+  defp sanitize([fl | tail], acc_list) do
     case fl do
-      ?ä -> sanitize(tail, [?e | [?a | rest]])
-      ?ö -> sanitize(tail, [?e | [?o | rest]])
-      ?ü -> sanitize(tail, [?e | [?u | rest]])
-      ?ß -> sanitize(tail, [?s | [?s | rest]])
-      ?_ -> sanitize(tail, [fl | rest])
-      _  -> sanitize(tail, rest)
+      ?ä -> sanitize(tail, [?e | [?a | acc_list]])
+      ?ö -> sanitize(tail, [?e | [?o | acc_list]])
+      ?ü -> sanitize(tail, [?e | [?u | acc_list]])
+      ?ß -> sanitize(tail, [?s | [?s | acc_list]])
+      ?_ -> sanitize(tail, [fl | acc_list])
+      _  -> sanitize(tail, acc_list)
     end
-  end
-
-  defp sanitize([], rest) do
-    rest
-      |> Enum.reverse()
   end
 end
