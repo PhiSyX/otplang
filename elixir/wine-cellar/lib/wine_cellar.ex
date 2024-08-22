df3fbf115cdb212f792829b8ec0565bd42a87a71
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2024-08-22 14:10:01 +0200

    exercism(elixir): Wine Cellar

---
+++ elixir/wine-cellar/lib/wine_cellar.ex
@@ -0,0 +1,52 @@
+defmodule WineCellar do
+  def explain_colors do
+    [
+      white: "Fermented without skin contact.",
+      red: "Fermented with skin contact using dark-colored grapes.",
+      rose: "Fermented with some skin contact, but not enough to qualify as a red wine."
+    ]
+  end
+
+  def filter(cellar, color, opts \\ []) do
+    Keyword.get_values(cellar, color)
+      |> filter_opt(opts)
+  end
+
+  defp filter_opt(cellar, []), do: cellar
+
+  defp filter_opt(cellar, [{:country, country} | tail]) do
+    cellar
+      |> filter_by_country(country)
+      |> filter_opt(tail)
+  end
+
+  defp filter_opt(cellar, [{:year, year} | tail]) do
+    cellar
+      |> filter_by_year(year)
+      |> filter_opt(tail)
+  end
+
+  # The functions below do not need to be modified.
+
+  defp filter_by_year(wines, year)
+  defp filter_by_year([], _year), do: []
+
+  defp filter_by_year([{_, year, _} = wine | tail], year) do
+    [wine | filter_by_year(tail, year)]
+  end
+
+  defp filter_by_year([{_, _, _} | tail], year) do
+    filter_by_year(tail, year)
+  end
+
+  defp filter_by_country(wines, country)
+  defp filter_by_country([], _country), do: []
+
+  defp filter_by_country([{_, _, country} = wine | tail], country) do
+    [wine | filter_by_country(tail, country)]
+  end
+
+  defp filter_by_country([{_, _, _} | tail], country) do
+    filter_by_country(tail, country)
+  end
+end
